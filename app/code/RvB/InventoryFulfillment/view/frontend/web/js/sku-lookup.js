d376f3ecf71bb5cd88227c2d5c2d4acfc48c0683
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-17 21:20:34 -0600

    Se agregan defaults al componente skuLookup, se comenta nodo config del layout.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -4,6 +4,10 @@ define([
     'use strict';
     
     return Component.extend({
+        defaults: {
+            template: 'RvB_InventoryFulfillment/sku-lookup',
+            sku: 'ABC123'
+        },
         initialize() {
             this._super();
             console.log('The skuLookup component has been loaded');
