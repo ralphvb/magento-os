e66f5676707723f1a1ff44a8a5ab1c566d2c650e
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-17 23:57:42 -0600

    Se agrega knockout para manejar variables observables.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -1,12 +1,14 @@
 define([
-    'uiComponent'
-], function(Component) {
+    'uiComponent',
+    'ko'
+], function(Component, ko) {
     'use strict';
     
     return Component.extend({
         defaults: {
             template: 'RvB_InventoryFulfillment/sku-lookup',
-            sku: 'ABC123'
+            sku: ko.observable('ABC123'),
+            placeholder: ko.observable('Example: 24-MB01')
         },
         initialize() {
             this._super();
