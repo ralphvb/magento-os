4e0bef6b3fb5824539d4bf169a09e827b539c4fb
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-18 20:57:07 -0600

    Se agrega jQuery para iniciar y terminar loader al hacer peticion ajax.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -1,8 +1,9 @@
 define([
     'uiComponent',
     'ko',
-    'mage/storage'
-], function (Component, ko, storage) {
+    'mage/storage',
+    'jquery'
+], function (Component, ko, storage, $) {
     'use strict';
 
     return Component.extend({
@@ -10,22 +11,29 @@ define([
             template: 'RvB_InventoryFulfillment/sku-lookup',
             sku: ko.observable('24-MB01'),
             placeholder: ko.observable('Example: 24-MB01'),
-            messageResponse: ko.observable('')
+            messageResponse: ko.observable(''),
+            isSuccess: ko.observable(false)
         },
         initialize() {
             this._super();
             console.log('The skuLookup component has been loaded');
         },
         handleSubmit() {
+            $('body').trigger('processStart');
             this.messageResponse('');
-            
+            this.isSuccess(false);
+
             storage.get(`rest/V1/products/${this.sku()}`)
                 .done(response => {
                     this.messageResponse(`Product found! <strong>${response.name}</strong>`);
-                    console.log(response);
+                    this.isSuccess(true);
                 })
                 .fail(() => {
                     this.messageResponse('Product not found!');
+                    this.isSuccess(false);
+                })
+                .always(() => {
+                    $('body').trigger('processStop');
                 });
         }
     });
