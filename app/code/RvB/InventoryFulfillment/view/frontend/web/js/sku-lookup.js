17e865f23a9dbebe0fe8b82749998c333d4eda60
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-18 20:49:02 -0600

    Se agrega logica para mostrar mensaje de exito o error al usuario.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -2,23 +2,31 @@ define([
     'uiComponent',
     'ko',
     'mage/storage'
-], function(Component, ko, storage) {
+], function (Component, ko, storage) {
     'use strict';
-    
+
     return Component.extend({
         defaults: {
             template: 'RvB_InventoryFulfillment/sku-lookup',
             sku: ko.observable('24-MB01'),
-            placeholder: ko.observable('Example: 24-MB01')
+            placeholder: ko.observable('Example: 24-MB01'),
+            messageResponse: ko.observable('')
         },
         initialize() {
             this._super();
             console.log('The skuLookup component has been loaded');
         },
         handleSubmit() {
-            storage.get(`rest/V1/products/${this.sku()}`).done(response => {
-                console.log(response);
-            });
+            this.messageResponse('');
+            
+            storage.get(`rest/V1/products/${this.sku()}`)
+                .done(response => {
+                    this.messageResponse(`Product found! <strong>${response.name}</strong>`);
+                    console.log(response);
+                })
+                .fail(() => {
+                    this.messageResponse('Product not found!');
+                });
         }
     });
 });
