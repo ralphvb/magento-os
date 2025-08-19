d373001f1dc99e723c9844c60d64a2b712287e06
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-18 21:44:45 -0600

    Se agrega logica para el manejo un array observable.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/box-configurations.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/box-configurations.js
@@ -1,9 +1,13 @@
 define([
-    'uiComponent'
-], function(Component) {
+    'uiComponent',
+    'ko'
+], function(Component, ko) {
     'use strict';
 
     return Component.extend({
+        defaults: {
+            boxConfigurations: ko.observableArray(['abc', 'def'])
+        },
         initialize() {
             this._super();
             console.log('The boxConfigurations component has been loaded!');
