35f26c22a0dabc6c9851338926bedadd996fcfbd
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-18 20:43:38 -0600

    Se agrega mage/storage para hacer peticiones AJAX.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/sku-lookup.js
@@ -1,13 +1,14 @@
 define([
     'uiComponent',
-    'ko'
-], function(Component, ko) {
+    'ko',
+    'mage/storage'
+], function(Component, ko, storage) {
     'use strict';
     
     return Component.extend({
         defaults: {
             template: 'RvB_InventoryFulfillment/sku-lookup',
-            sku: ko.observable('ABC123'),
+            sku: ko.observable('24-MB01'),
             placeholder: ko.observable('Example: 24-MB01')
         },
         initialize() {
@@ -15,7 +16,9 @@ define([
             console.log('The skuLookup component has been loaded');
         },
         handleSubmit() {
-            console.log(this.sku() + ' SKU confirmed!');
+            storage.get(`rest/V1/products/${this.sku()}`).done(response => {
+                console.log(response);
+            });
         }
     });
 });
