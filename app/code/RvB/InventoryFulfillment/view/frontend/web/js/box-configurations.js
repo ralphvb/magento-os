18bf38de1b403e2fbf9fd1d811ed939af9e7ec10
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-19 21:46:14 -0600

    Manejo de suscripciones con ko.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/box-configurations.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/box-configurations.js
@@ -1,17 +1,24 @@
 define([
     'uiComponent',
     'ko',
-    'RvB_InventoryFulfillment/js/model/box-configurations'
-], function (Component, ko, boxConfigurationsModel) {
+    'RvB_InventoryFulfillment/js/model/box-configurations',
+    'RvB_InventoryFulfillment/js/model/sku'
+], function (Component, ko, boxConfigurationsModel, skuModel) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            boxConfigurationsModel: boxConfigurationsModel
+            boxConfigurationsModel: boxConfigurationsModel,
         },
         initialize() {
             this._super();
             console.log('The boxConfigurations component has been loaded!');
+            skuModel.isSuccess.subscribe((value) => {
+                console.log('SKU isSuccess new value', value);
+            });
+            skuModel.isSuccess.subscribe((value) => {
+                console.log('SKU isSuccess old value', value);
+            }, null, 'beforeChange');
         },
         handleAdd() {
             boxConfigurationsModel.add();
