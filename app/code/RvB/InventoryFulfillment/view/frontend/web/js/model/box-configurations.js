ee3d5aedbd9450cfc59ef5074d614f923c23d83a
Rafael Contreras Herrera <rjcch90@outlook.com>
2025-08-19 21:56:43 -0600

    Manejo de computed functions con ko.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/model/box-configurations.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/model/box-configurations.js
@@ -3,7 +3,8 @@ define([
 ], function (ko) {
 
     const boxConfiguration = () => {
-        return {
+        const divisor = 139;
+        const data = {
             length: ko.observable(),
             width: ko.observable(),
             height: ko.observable(),
@@ -11,6 +12,13 @@ define([
             unitsPerBox: ko.observable(),
             numberOfBoxes: ko.observable(),
         };
+
+        data.dimensionalWeight = ko.computed(() => {
+            const result = data.length() * data.width() * data.height() / divisor;
+            return Math.round(result * data.numberOfBoxes());
+        });
+
+        return data;
     }
 
     return {
