35abb8d23b11f5bf7b4ca86a1252020ef3714a2b
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-02 16:57:01 -0600

    Se definen defaults en Free Shipping Component.

--- app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -4,9 +4,13 @@ define([
     'use strict';
 
     return Component.extend({
+        defaults: {
+            message: 'Free Shipping Message'
+        },
         initialize: function () {
             this._super();
             console.log("Free Shipping Banner Ui Component has been loaded!");
+            console.log(this.message);
         }
     });
 });
