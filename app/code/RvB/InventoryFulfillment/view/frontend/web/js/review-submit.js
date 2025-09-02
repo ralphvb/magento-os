39fbb3258978329f7ad0438c97d0ff0d67e5b212
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-02 12:00:38 -0600

    Se agrega funcionalidad para Review Component.

--- app/code/RvB/InventoryFulfillment/view/frontend/web/js/review-submit.js
+++ app/code/RvB/InventoryFulfillment/view/frontend/web/js/review-submit.js
@@ -1,14 +1,31 @@
 define([
     'uiComponent',
     'ko',
+    'RvB_InventoryFulfillment/js/model/sku',
     'RvB_InventoryFulfillment/js/model/box-configurations',
-], function(Component, ko, boxConfigurationsModel){
+], function(Component, ko, skuModel, boxConfigurationsModel){
     'use strict';
 
     return Component.extend({
+        defaults: {
+            numberOfBoxes: 0,
+            shipmentWeight: 0,
+            billableWeight: 0,
+            isTermsChecked: ko.observable(false),
+        },
         initialize() {
             this._super();
             console.log('The reviewSubmit component has been loaded!');
+            this.canSubmit = ko.computed(() => {
+                return skuModel.isSuccess() && boxConfigurationsModel.isSuccess() && this.isTermsChecked();
+            })
+        },
+        handleSubmit() {
+            if(this.canSubmit()){
+                console.log('The Review Submit has been submitted!');
+            } else {
+                console.log('The Review Submit has an error!');
+            }
         }
     })
 })
