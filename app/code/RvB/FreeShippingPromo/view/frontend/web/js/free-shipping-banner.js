db81cd891f446d729dda1f2426dd4058cbc75490
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-04 15:15:37 -0600

    Se agrega obtencion del cart mediante customerData y su dependencia al modulo.

--- app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -1,19 +1,25 @@
 define([
-    'uiComponent'
-], function (Component) {
+    'uiComponent',
+    'Magento_Customer/js/customer-data'
+], function (Component, customerData) {
     'use strict';
 
     return Component.extend({
         defaults: {
             template: 'RvB_FreeShippingPromo/free-shipping-banner',
-            subtotal: 33.00,
+            subtotal: 0.00,
             tracks: {
                 subtotal: true
             }
         },
         initialize: function () {
             this._super();
-            console.log(this.message);
+            var self = this;
+            var cart = customerData.get('cart');
+
+            customerData.getInitCustomerData().done(function() {
+                self.subtotal = formatCurrency(cart().subtotalAmount);
+            });
         },
         formatCurrency: function(value) {
             return '$' +  value.toFixed(2);
