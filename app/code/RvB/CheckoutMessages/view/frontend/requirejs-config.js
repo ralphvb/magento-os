79a54a01f3ed0db2b26019f72f1242434d7d6c6e
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-04 17:16:59 -0600

    Se agrega metodo Static Path Mapping Override para templates.

--- app/code/RvB/CheckoutMessages/view/frontend/requirejs-config.js
+++ app/code/RvB/CheckoutMessages/view/frontend/requirejs-config.js
@@ -5,5 +5,11 @@ var config = {
                 'RvB_CheckoutMessages/js/view/summary/cart-items-mixin': true
             }
         }
+    },
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar':
+                'RvB_CheckoutMessages/template/sidebar'
+        }
     }
 }
