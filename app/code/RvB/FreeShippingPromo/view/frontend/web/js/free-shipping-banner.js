4a93ee411cc1b2b6989e4415ec9d48b16556d0a6
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-04 15:38:56 -0600

    Se agrega suscripcion al cart para actualizar valor del subtotal reactivamente.

--- app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
+++ app/code/RvB/FreeShippingPromo/view/frontend/web/js/free-shipping-banner.js
@@ -18,14 +18,20 @@ define([
             var self = this;
             var cart = customerData.get('cart');
 
-            customerData.getInitCustomerData().done(function() {
-                if(!_.isEmpty(cart()) && !_.isUndefined(cart().subtotalAmount)) {
+            customerData.getInitCustomerData().done(function () {
+                if (!_.isEmpty(cart()) && !_.isUndefined(cart().subtotalAmount)) {
                     self.subtotal = parseFloat(cart().subtotalAmount);
                 }
             });
+
+            cart.subscribe(function (cart) {
+                if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
+                    self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            })
         },
-        formatCurrency: function(value) {
-            return '$' +  value.toFixed(2);
+        formatCurrency: function (value) {
+            return '$' + value.toFixed(2);
         }
     });
 });
