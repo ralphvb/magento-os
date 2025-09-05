ce00e97e0a3dc0dcc86d65430322334160b2676f
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-09-05 11:20:17 -0600

    Manejo del deps para cargar un componente hasta que otros se carguen.

--- app/code/RvB/CheckoutMessages/view/frontend/web/js/view/summary/guarantee.js
+++ app/code/RvB/CheckoutMessages/view/frontend/web/js/view/summary/guarantee.js
@@ -4,6 +4,10 @@ define([
     'use strict';
 
     return Component.extend({
+        initialize: function() {
+            this._super();
+            console.log(this.name + ' is initialized.');
+        },
         showMessage: function () {
             return this.subtotal > 100;
         },
--- app/code/RvB/CheckoutMessages/view/frontend/web/js/view/summary/shipping-time.js
+++ app/code/RvB/CheckoutMessages/view/frontend/web/js/view/summary/shipping-time.js
@@ -9,12 +9,16 @@ define([
                 countryId: 'checkoutProvider:shippingAddress.country_id'
             },
             listens: {
-                // 'checkoutProvider:shippingAddress.country_id': 'countryId',
+                // 'checkoutProvider:shippingAddress.country_id': 'countryId', Same as imports
                 'checkoutProvider:shippingAddress.region_id': 'handleRegionChange'
             },
             tracks: {
                 countryId: true
             }
+        },
+        initialize: function() {
+            this._super();
+            console.log(this.name + ' is initialized.');
         },
         showMessage: function() {
             return this.countryId === 'US';
