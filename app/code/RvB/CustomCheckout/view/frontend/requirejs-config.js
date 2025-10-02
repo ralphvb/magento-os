ffb62883d587ad800996670248ebbcdb4aa50649
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-02 15:16:24 -0600

    Metodo alternativo para hacer override a ko templates.

--- app/code/RvB/CustomCheckout/view/frontend/requirejs-config.js
+++ app/code/RvB/CustomCheckout/view/frontend/requirejs-config.js
@@ -1,5 +1,11 @@
 let config = {
     deps: [
         'RvB_CustomCheckout/js/mask-telephone-inputs'
-    ]
+    ],
+    map: {
+        '*': {
+            'Magento_Checkout/template/sidebar.html':
+                'RvB_CustomCheckout/template/sidebar.html'
+        }
+    }
 }
