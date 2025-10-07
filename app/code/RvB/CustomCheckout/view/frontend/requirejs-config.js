e5701e04fc69cf74190e224c11bf68a810a4e8ce
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-07 15:27:39 -0600

    Mixin para vincular customAttribute como extensionAttribute en shippingAddress.

--- app/code/RvB/CustomCheckout/view/frontend/requirejs-config.js
+++ app/code/RvB/CustomCheckout/view/frontend/requirejs-config.js
@@ -7,5 +7,12 @@ let config = {
             'Magento_Checkout/template/sidebar.html':
                 'RvB_CustomCheckout/template/sidebar.html'
         }
+    },
+    config: {
+        mixins: {
+            'Magento_Checkout/js/action/set-shipping-information': {
+                'RvB_CustomCheckout/js/action/set-shipping-information-mixin': true
+            }
+        }
     }
 }
