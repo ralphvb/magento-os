2859d8a3a47b92d7be7114d40a352d24ba3de948
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-07 09:34:34 -0600

    Mostrar un CMS Block en el checkout con configProvider.

--- app/code/RvB/CustomCheckout/Model/CheckoutConfigProvider.php
+++ app/code/RvB/CustomCheckout/Model/CheckoutConfigProvider.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace RvB\CustomCheckout\Model;
 
+use Magento\Framework\View\LayoutInterface;
+
 class CheckoutConfigProvider implements \Magento\Checkout\Model\ConfigProviderInterface
 {
+    private $fulfillmentStatus;
+
+    public function __construct(LayoutInterface $layoutInterface)
+    {
+        $this->fulfillmentStatus = $layoutInterface->createBlock('Magento\Cms\Block\Block')->setBlockId('fulfillment_status')->toHtml();
+    }
 
     public function getConfig() : array
     {
         return [
-            'myKey' => 'myValue'
+            'fullfilment_status' => $this->fulfillmentStatus
         ];
     }
 }
