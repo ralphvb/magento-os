27674cd48bde6b7a5b478817a51a94e033e8d8e1
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-09 10:33:06 -0600

    Se elimina Address Classification del Billing Address Form en Checkout.

--- app/code/RvB/CustomCheckout/Block/Checkout/LayoutProcessor/AddressClassificationAttribute.php
+++ app/code/RvB/CustomCheckout/Block/Checkout/LayoutProcessor/AddressClassificationAttribute.php
@@ -15,6 +15,20 @@ class AddressClassificationAttribute implements \Magento\Checkout\Block\Checkout
         $attributeData['config']['customScope'] = 'shippingAddress.custom_attributes';
         $attributeData['dataScope'] = "shippingAddress.custom_attributes.$attributeCode";
 
+        foreach (
+            $jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['payments-list']['children'] as &$paymentMethod
+        ) {
+            $fields = &$paymentMethod['children']['form-fields']['children'];
+
+            if (isset($fields[$attributeCode])) {
+                unset($fields[$attributeCode]);
+            }
+
+            $fields['city']['sortOrder'] = '72';
+            $fields['region_id']['sortOrder'] = '74';
+            $fields['postcode']['sortOrder'] = '76';
+        };
+
         return $jsLayout;
     }
 }
