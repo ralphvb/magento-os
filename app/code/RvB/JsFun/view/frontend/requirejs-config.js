b8fc468cb532c0d2b73d03b5f457470d03a5fc90
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-17 11:54:08 -0600

    Uso de map y path para hacer referencia a archivos de codigo por alias y no por ruta.

---
+++ app/code/RvB/JsFun/view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+var config = {
+    "map": {
+        "*": {
+            "fadeInElement": "RvB_JsFun/js/fade-in-element"
+        }
+    },
+    "paths": {
+        // "vue": "RvB_JsFun/js/vue"
+        "vue": "https://cdn.jsdelivr.net/npm/vue@2.7.16"
+    }
+}
