51ab22c107c8970c5d06af3d75a44ee3694edb3a
Rafael Contreras Herrera <rafaelcontreras@wolfsellers.com>
2025-10-17 13:04:21 -0600

    Manejo de ViewModel.

---
+++ app/code/RvB/JsFun/ViewModel/Message.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RvB\JsFun\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class Message implements ArgumentInterface {
+    public function getMessage() {
+        return str_shuffle('Declarative test');
+    }
+}
